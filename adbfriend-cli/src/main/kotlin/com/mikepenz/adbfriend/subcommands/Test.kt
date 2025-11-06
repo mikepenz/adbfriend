a92cb26c689be89df6f64314d03fd35b9f196b89
Mike Penz <opensource@mikepenz.dev>
2025-11-06 12:29:01 +0100

    Refactor stay-on option to use switch for improved clarity and usability

--- adbfriend-cli/src/main/kotlin/com/mikepenz/adbfriend/subcommands/Test.kt
+++ adbfriend-cli/src/main/kotlin/com/mikepenz/adbfriend/subcommands/Test.kt
@@ -5,6 +5,7 @@ import com.github.ajalt.clikt.core.terminal
 import com.github.ajalt.clikt.parameters.options.flag
 import com.github.ajalt.clikt.parameters.options.help
 import com.github.ajalt.clikt.parameters.options.option
+import com.github.ajalt.clikt.parameters.options.switch
 import com.github.ajalt.mordant.terminal.YesNoPrompt
 import com.malinskiy.adam.request.device.Device
 import com.malinskiy.adam.request.shell.v2.ShellCommandRequest
@@ -17,7 +18,10 @@ class Test : AdbCommand() {
     private val touches: Boolean by option().flag().help("Also enables touches when configuring for tests (`--reset` will disable again)")
     private val unlock: Boolean by option().flag().help("Attempts to unlock the device by sending (`keyevent 82`)")
     private val collapse: Boolean by option().flag().help("Attempts to collapse the statusbar")
-    private val stayOn: Boolean? by option().flag("--no-stay-on").help("Keeps device awake while USB connected (`svc power stayon usb`). Use --stay-on to enable, --no-stay-on to disable.")
+    private val stayOn: Boolean? by option().switch(
+        "--stay-on" to true,
+        "--no-stay-on" to false
+    ).help("Keeps device awake while USB connected (`svc power stayon usb`). Use --stay-on to enable, --no-stay-on to disable.")
     private val force: Boolean by option().flag().help("Skips all warning prompts, and applies settings without confirmation.")
 
     override fun help(context: Context) = """
